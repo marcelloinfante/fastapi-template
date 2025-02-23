aa45610e2d9d23b19e602904d439206e8ccb9742
Marcello Infante <marcelloinfantee@gmail.com>
2025-02-22 22:59:21 -0300

    feat: add default setting values

--- app/config.py
+++ app/config.py
@@ -1,18 +1,24 @@
+from enum import Enum
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-from dotenv import load_dotenv
 
 load_dotenv()
 
 
+class EnvironmentEnum(str, Enum):
+    PRODUCTION = "PRODUCTION"
+    DEVELOPMENT = "DEVELOPMENT"
+
+
 class Settings(BaseSettings):
-    secret_key: str
-    environment: str
-    allow_origin: str
-    database_url: str
-    hash_algorithm: str
-    admin_secret_key: str
-    access_token_expire_minutes: int
+    secret_key: str = "123456789"
+    allow_origin: str = "*"
+    environment: EnvironmentEnum = EnvironmentEnum.DEVELOPMENT
+    database_url: str = "postgresql://fastapi:fastapi@db:5432/fastapi"
+    hash_algorithm: str = "HS256"
+    admin_secret_key: str = "360"
+    access_token_expire_minutes: int = 360
 
 
 settings = Settings()
